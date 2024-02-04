6d33dc1b73142bb3dcc115c8a83f3f6cfaae47d8
Kamiichi <paperone430@gmail.com>
2024-02-04 23:59:35 +0900

    modify hello2.sh to display script name

--- MasteringLinuxShellScript/chapter1/hello2.sh
+++ MasteringLinuxShellScript/chapter1/hello2.sh
@@ -2,5 +2,8 @@
 # $1は位置引数。スクリプトに渡された1番目(最初)の引数を表す。
 # echo "Hello $1"
 # $*はスクリプトに渡されるすべての引数を表す。
+echo "You are using $0"
+echo "Um..., I mean"
+echo "You are using $(basename $0)"
 echo "Hello $*"
 exit 0
