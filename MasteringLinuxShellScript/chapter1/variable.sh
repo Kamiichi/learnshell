7b0ac6aa604731ac922f2063dfdc76d7eaef951b
Kamiichi <paperone430@gmail.com>
2024-02-06 00:33:32 +0900

    modify variable.sh shell can use arr variants

--- MasteringLinuxShellScript/chapter1/variable.sh
+++ MasteringLinuxShellScript/chapter1/variable.sh
@@ -1,7 +1,11 @@
 #!/bin/zsh
+# myarr=({1..10}) ブレーズ展開も配列に格納できたね
+myarr=(one two three four five)
 name="Mokhtar"
 age=35
 total=16.5
 echo $name
 echo $age
 echo $total
+echo ${myarr[1]}
+echo ${myarr[2]}
